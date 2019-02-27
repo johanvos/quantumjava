ecfb952aae6eb0bca2433c40dae7611ea4f4fcd2
Johan Vos <johan.vos@gluonhq.com>
2019-02-27 20:34:10 +0100

    import first sample

---
+++ ch02/hellostrange/src/main/java/com/gluonhq/javaqc/ch02/hellostrange/Main.java
@@ -0,0 +1,23 @@
+package com.gluonhq.javaqc.ch02.hellostrange;
+
+import com.gluonhq.strange.algorithm.Classic;
+
+public class Main {
+
+    public static void main (String[] args) {
+        System.out.println("Using high-level Strange API to generate random bits");
+        System.out.println("----------------------------------------------------");
+        int randomBit = Classic.randomBit();
+        System.out.println("Generate one random bit, which can be 0 or 1. Result = "+randomBit);
+        int cntZero = 0;
+        int cntOne = 0;
+        for (int i = 0; i < 10000000; i++) {
+            if (Classic.randomBit() > 0) {
+                cntOne ++;
+            } else {
+                cntZero ++;
+            }
+        }
+        System.out.println("Generated 10000 random bits, "+cntZero+" of them were 0, and "+cntOne+" were 1.");
+    }
+}
