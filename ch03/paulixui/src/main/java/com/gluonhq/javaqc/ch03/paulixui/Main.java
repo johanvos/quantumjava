4f3ecaf4c4d4ee261074252e705bda7244f5cd26
Johan Vos <johan@lodgon.com>
2019-02-28 12:31:21 +0100

    add chapter 3

---
+++ ch03/paulixui/src/main/java/com/gluonhq/javaqc/ch03/paulixui/Main.java
@@ -0,0 +1,25 @@
+package com.gluonhq.javaqc.ch03.paulixui;
+
+import com.gluonhq.strange.*;
+import com.gluonhq.strange.gate.*;
+import com.gluonhq.strange.local.*;
+import com.gluonhq.strangefx.render.Renderer;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        QuantumExecutionEnvironment simulator = new SimpleQuantumExecutionEnvironment();
+        Program program = new Program(1);
+        Step step = new Step();
+        step.addGate(new X(0));
+        program.addStep(step);
+        Result result = simulator.runProgram(program);
+        Qubit[] qubits = result.getQubits();
+        Qubit zero = qubits[0];
+        int value = zero.measure();
+        System.out.println("Value = "+value);
+        Renderer.renderProgram(program);
+    }  
+
+}
