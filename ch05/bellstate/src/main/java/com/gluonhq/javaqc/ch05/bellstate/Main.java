722843bb5b2d3225d9d5c1c80001e782afd159a1
Johan Vos <johan@lodgon.com>
2019-05-01 21:59:42 +0200

    add Bell state sample (chapter 5)

---
+++ ch05/bellstate/src/main/java/com/gluonhq/javaqc/ch05/bellstate/Main.java
@@ -0,0 +1,27 @@
+package com.gluonhq.javaqc.ch05.bellstate;
+
+import com.gluonhq.strange.*;
+import com.gluonhq.strange.gate.*;
+import com.gluonhq.strange.local.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        QuantumExecutionEnvironment simulator = new SimpleQuantumExecutionEnvironment();
+        Program program = new Program(2);
+        Step step1 = new Step();
+        step1.addGate(new Hadamard(0));
+        program.addStep(step1);
+        Step step2 = new Step();
+        step2.addGate(new Cnot(0,1));
+        program.addStep(step2);
+        Result result = simulator.runProgram(program);
+        Qubit[] qubits = result.getQubits();
+        Qubit q0 = qubits[0];
+        Qubit q1 = qubits[1];
+        int v0 = q0.measure();
+        int v1 = q1.measure();
+        System.out.println("v0 = "+v0+" and v1 = "+v1);
+    }  
+
+}
