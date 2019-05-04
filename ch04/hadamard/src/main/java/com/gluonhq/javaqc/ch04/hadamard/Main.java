9f6edc89334bb4ec4edc0b92308e70a74e0c5b57
Johan Vos <johan@lodgon.com>
2019-05-04 18:00:23 +0200

    add function that runs the sample 1000 times

--- ch04/hadamard/src/main/java/com/gluonhq/javaqc/ch04/hadamard/Main.java
+++ ch04/hadamard/src/main/java/com/gluonhq/javaqc/ch04/hadamard/Main.java
@@ -7,6 +7,18 @@ import com.gluonhq.strange.local.*;
 public class Main {
 
     public static void main(String[] args) {
+        System.err.println("==================================================");
+        System.err.println("Single run of a Quantum Circuit with Hadamard Gate");
+        singleExecution();
+        System.err.println("==================================================");
+        System.err.println("\n\n");
+        System.err.println("==================================================");
+        System.err.println("1000 runs of a Quantum Circuit with Hadamard Gate");
+        manyExecution();
+        System.err.println("==================================================");
+    }
+
+    public static void singleExecution() {
         QuantumExecutionEnvironment simulator = new SimpleQuantumExecutionEnvironment();
         Program program = new Program(1);
         Step step = new Step();
@@ -17,6 +29,26 @@ public class Main {
         Qubit zero = qubits[0];
         int value = zero.measure();
         System.out.println("Value = "+value);
+    }  
+
+    public static void manyExecution() {
+        QuantumExecutionEnvironment simulator = new SimpleQuantumExecutionEnvironment();
+        Program program = new Program(1);
+        Step step = new Step();
+        step.addGate(new Hadamard(0));
+        program.addStep(step);
+        int cntZero = 0;
+        int cntOne = 0;
+        for (int i = 0; i < 1000; i++) {
+            Result result = simulator.runProgram(program);
+            Qubit[] qubits = result.getQubits();
+            Qubit zero = qubits[0];
+            int value = zero.measure();
+            if (value == 0) cntZero++;
+            if (value == 1) cntOne++;
+        }
+        System.out.println("Applied Hadamard circuit 1000 times, got "
+                + cntZero + " times 0 and " + cntOne + " times 1.");
     }  
 
 }
