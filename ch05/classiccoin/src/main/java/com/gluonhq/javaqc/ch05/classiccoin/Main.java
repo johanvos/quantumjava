4ba6c96326b324d50138235e055f9a03615e9c16
Johan Vos <johan@lodgon.com>
2019-07-20 12:52:26 +0200

    include rendering code into Main class

--- ch05/classiccoin/src/main/java/com/gluonhq/javaqc/ch05/classiccoin/Main.java
+++ ch05/classiccoin/src/main/java/com/gluonhq/javaqc/ch05/classiccoin/Main.java
@@ -1,5 +1,17 @@
 package com.gluonhq.javaqc.ch05.classiccoin;
 
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 public class Main {
 
     private static final int count = 1000;
@@ -16,7 +28,37 @@ public class Main {
         System.out.println("0 1 occured "+results[1]+" times.");
         System.out.println("1 0 occured "+results[2]+" times.");
         System.out.println("1 1 occured "+results[3]+" times.");
+        Platform.startup(() -> showResults(results));
     }
 
+    private static void showResults(int[] results) {
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+        BarChart<String, Integer> barChart = new BarChart(xAxis, yAxis);
+        barChart.setData(getChartData(results));
+        StackPane root = new StackPane();
+        root.getChildren().add(barChart);
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root, 640, 480));
+        stage.show();
+    }
+
+    private static ObservableList<XYChart.Series<String, Integer>> getChartData(int[] results) {
+        ObservableList<XYChart.Series<String, Integer>> answer = FXCollections.observableArrayList();
+        XYChart.Series<String, Integer> series = new XYChart.Series<>();
+        answer.add(series);
+        for (int i = 0; i < results.length;i++) {
+            series.getData().add(new XYChart.Data<>(getFixedBinaryString(i, (int) (Math.log(results.length)/Math.log(2))), results[i]));
+        }
+        return answer;
+    }
+
+    private static String getFixedBinaryString(int i, int w) {
+        StringBuffer buff = new StringBuffer(Integer.toBinaryString(i));
+        while (buff.length() < w) {
+            buff.insert(0, "0");
+        }
+        return buff.toString();
+    }
 
 }
