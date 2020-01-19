fd9b27c133bd5bce92695e7bb2a72f40625239f8
Johan Vos <johan@lodgon.com>
2020-01-19 17:25:04 +0100

    add example showing the impact of (wrong) guesses by Eve

---
+++ ch08/guess/src/main/java/com/gluonhq/javaqc/ch08/guess/Main.java
@@ -0,0 +1,50 @@
+package com.gluonhq.javaqc.ch08.guess;
+
+import com.gluonhq.strange.*;
+import com.gluonhq.strange.gate.*;
+import com.gluonhq.strange.local.*;
+import com.gluonhq.strangefx.render.*;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final int SIZE = 8;
+        Random random = new Random();
+
+        boolean[] aliceBits = new boolean[SIZE];
+
+        QuantumExecutionEnvironment simulator = new SimpleQuantumExecutionEnvironment();
+        Program program = new Program(SIZE);
+        Step prepareStep = new Step();
+        Step superPositionStep = new Step();
+        Step superPositionStep2 = new Step();
+        Step measureStep = new Step();
+        for (int i = 0; i < SIZE; i++) {
+            if (i > (SIZE/2-1)) prepareStep.addGate(new X(i));
+            if ( (i/2) % 2 == 1) superPositionStep.addGate(new Hadamard(i));
+            if (i%2 ==1 )superPositionStep2.addGate(new Hadamard(i));
+            measureStep.addGate(new Measurement(i));
+        }
+
+        program.addStep(prepareStep);
+        program.addStep(superPositionStep);
+        program.addStep(superPositionStep2);
+        program.addStep(measureStep);
+
+        Result result = simulator.runProgram(program);
+        Qubit[] qubit = result.getQubits();
+
+        int[] measurement = new int[SIZE];
+        boolean[] bobBits = new boolean[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            measurement[i] = qubit[i].measure();
+            bobBits[i] = measurement[i] == 1;
+            System.err.println("Alice sent "+(aliceBits[i] ? "1" : "0") + " and Bob received "+(bobBits[i] ? "1" : "0"));
+        }
+
+        Renderer.renderProgram(program);
+    }
+
+}
