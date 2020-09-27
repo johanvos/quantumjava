88ce97807f84a47bea0e3dfb268c80cef0957352
Johan Vos <johan@lodgon.com>
2020-09-27 14:53:22 +0200

    update versions

--- ch03/paulix/src/main/java/com/gluonhq/javaqc/ch03/paulix/Main.java
+++ ch03/paulix/src/main/java/com/gluonhq/javaqc/ch03/paulix/Main.java
@@ -1,8 +1,8 @@
 package com.gluonhq.javaqc.ch03.paulix;
 
-import com.gluonhq.strange.*;
-import com.gluonhq.strange.gate.*;
-import com.gluonhq.strange.local.*;
+import org.redfx.strange.*;
+import org.redfx.strange.gate.*;
+import org.redfx.strange.local.*;
 
 public class Main {
 
