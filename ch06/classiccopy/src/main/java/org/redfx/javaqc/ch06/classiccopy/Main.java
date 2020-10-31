f3045c46f5cfbe1e5411e2446e69287ef551bd81
Johan Vos <johan@lodgon.com>
2020-10-31 19:42:16 +0100

    make order of makeCopy and main consistent with the book

--- ch06/classiccopy/src/main/java/org/redfx/javaqc/ch06/classiccopy/Main.java
+++ ch06/classiccopy/src/main/java/org/redfx/javaqc/ch06/classiccopy/Main.java
@@ -2,16 +2,6 @@ package org.redfx.javaqc.ch06.classiccopy;
 
 public class Main {
 
-    public static void main(String[] args) {
-      Boolean trueSource = Boolean.TRUE;
-      Boolean trueCopy = makeCopy(trueSource);
-      System.err.println("Source: "+trueSource+" and copy : "+trueCopy);
-
-      Boolean falseSource = Boolean.FALSE;
-      Boolean falseCopy = makeCopy(falseSource);
-      System.err.println("Source: "+falseSource+" and copy : "+falseCopy);
-    }
-
     static Boolean makeCopy(Boolean source) {
         Boolean target;
         if (source == true) {
@@ -20,6 +10,16 @@ public class Main {
             target = Boolean.valueOf(false);
         }
         return target;
+    }
+
+    public static void main(String[] args) {
+      Boolean trueSource = Boolean.TRUE;
+      Boolean trueCopy = makeCopy(trueSource);
+      System.err.println("Source: "+trueSource+" and copy : "+trueCopy);
+
+      Boolean falseSource = Boolean.FALSE;
+      Boolean falseCopy = makeCopy(falseSource);
+      System.err.println("Source: "+falseSource+" and copy : "+falseCopy);
     }
 
 }
