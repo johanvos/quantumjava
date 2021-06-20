ed43318d6abe845cc175176a498d251cab9f1189
Johan Vos <johan@lodgon.com>
2021-06-20 10:40:41 +0200

    add solution for exercice in 6.3 Make sure copies of the Boolean objects are new instances

---
+++ ch06/hczmeasure/src/main/java/org/redfx/javaqc/ch06/hczmeasure/Main.java
@@ -0,0 +1,29 @@
+package org.redfx.javaqc.ch06.hczmeasure;
+
+import org.redfx.strange.*;
+import org.redfx.strange.gate.*;
+import org.redfx.strange.local.*;
+import org.redfx.strangefx.render.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        QuantumExecutionEnvironment simulator = new SimpleQuantumExecutionEnvironment();
+        Program program = new Program(2);
+        Step step1 = new Step();
+        step1.addGate(new Hadamard(0));
+        program.addStep(step1);
+        Step step2 = new Step();
+        step2.addGate(new Cz(0,1));
+        program.addStep(step2);
+        Result result = simulator.runProgram(program);
+        Qubit[] qubits = result.getQubits();
+        Qubit q0 = qubits[0];
+        Qubit q1 = qubits[1];
+        int v0 = q0.measure();
+        int v1 = q1.measure();
+        Renderer.renderProgram(program);
+        Renderer.showProbabilities(program, 1000);
+    }  
+
+}
